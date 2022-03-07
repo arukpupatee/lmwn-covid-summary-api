13491da19615423f4486c578f17011485539c34f
Aruk Pupatee <aruk.pupatee@gmail.com>
2022-03-08 05:58:17 +0700

    [Implement]: Get covid summary function in service to use data from external

--- server/service.go
+++ server/service.go
@@ -12,18 +12,44 @@ type Summary struct {
 	AgeGroup ageGroup
 }
 
+const unknownAgeValue = 0
+const youngUpperBoundValue = 30
+const middleUpperBoundValue = 60
+
 func getCovidSummary() Summary {
+	// TODO: Handle error
+	covidCaseResponseBody, _ := fetchCovidCase()
+	covidCaseData := covidCaseResponseBody.Data
+
+	provinceData := province{}
+	ageGroupData := ageGroup{}
+
+	for _, data := range covidCaseData {
+		province := data.ProvinceEn
+		age := data.Age
+
+		if province != "" {
+			if _, ok := provinceData[province]; !ok {
+				provinceData[province] = 1
+			} else {
+				provinceData[province]++
+			}
+		}
+
+		if age == unknownAgeValue {
+			ageGroupData.Unknown++
+		} else if age <= youngUpperBoundValue {
+			ageGroupData.Young++
+		} else if age <= middleUpperBoundValue {
+			ageGroupData.Middle++
+		} else {
+			ageGroupData.Old++
+		}
+	}
+
 	summary := Summary{
-		Province: map[string]int{
-			"Samut Sakhon": 3613,
-			"Bangkok":      2774,
-		},
-		AgeGroup: ageGroup{
-			Young:   300,
-			Middle:  150,
-			Old:     250,
-			Unknown: 4,
-		},
+		Province: provinceData,
+		AgeGroup: ageGroupData,
 	}
 
 	return summary
