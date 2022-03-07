b70e1700157683844e896be5422dcb39a1dcc345
Aruk Pupatee <aruk.pupatee@gmail.com>
2022-03-08 05:09:58 +0700

    [Implement]: Fetch covid case function

---
+++ external/external.go
@@ -0,0 +1,57 @@
+package external
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+type CovidCaseData struct {
+	ConfirmDate    string
+	Age            int
+	Gender         string
+	GenderEn       string // TODO: Refactor to enum
+	Nation         string
+	NationEn       string
+	Province       string
+	ProvinceEn     string
+	ProvinceId     int
+	District       string
+	StatQuarantine int
+}
+
+type CovidCaseResponseBody struct {
+	Data []CovidCaseData
+}
+
+func fetchCovidCase() (covidCaseResponseBody CovidCaseResponseBody, err error) {
+	covidCaseResponseBody = CovidCaseResponseBody{}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	httpClient := &http.Client{Transport: tr}
+
+	resp, err := httpClient.Get("https://static.wongnai.com/devinterview/covid-cases.json")
+
+	if err != nil {
+		return covidCaseResponseBody, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return covidCaseResponseBody, err
+	}
+
+	err = json.Unmarshal(body, &covidCaseResponseBody)
+
+	if err != nil {
+		return covidCaseResponseBody, err
+	}
+
+	return covidCaseResponseBody, nil
+}
